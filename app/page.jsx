e315e1b08a4296fb6c24645429de87b30aca9318
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2025-01-22 21:12:56 +0530

    let's start

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Button>Megh Deb</Button>
+    </div>
+  )
+}
+
+export default page
